7c3f85701584e283dd019217754485328b5ec588
hammerinformation <51826786+hammerinformation@users.noreply.github.com>
2022-01-17 19:26:48 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+#include "SimpleEvent.h"
+// https://github.com/hammerinformation/
+
+void print() {
+    std::cout << "Hello" << std::endl;
+}
+int main()
+{
+    SimpleEvent e;
+    e.AddListener(print);
+    e.AddListener(print);
+    e.Invoke();
+    return 0;
+}
